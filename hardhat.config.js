1ca510427ae7260394bdab58f40ed949dbc3595b
thomassdk <thomassdk@pm.me>
2022-05-31 08:22:29 +0200

    Added deployment target to sokol

--- hardhat.config.js
+++ hardhat.config.js
@@ -15,5 +15,10 @@ module.exports = {
       url: process.env.PROD_ALCHEMY_KEY,
       accounts: [process.env.PRIVATE_KEY],
     },
+    sokol: {
+      chainId: 77,
+      url: process.env.SOKOL_KEY,
+      accounts: [process.env.PRIVATE_KEY],
+    },
   },
 };
